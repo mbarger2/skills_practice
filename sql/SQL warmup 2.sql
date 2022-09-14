0648240d894c0b48fade844cc80ac13b510529f6
Matthew Barger <matthewbarger2@gmail.com>
2022-09-13 22:40:30 -0400

    MySql

--- sql/SQL warmup 2.sql
+++ sql/SQL warmup 2.sql
@@ -22,35 +22,17 @@ dayofweek(return_date) as return_weekday from rental LIMIT 20;
 select *, month(rental_date)as rental_month, dayname(rental_date) as rental_weekday, month(return_date)as return_month, 
 dayname(return_date) as return_weekday from rental LIMIT 20;
 
-NEXT TIME:;
-SELECT rental_date from rental limit 5;
-select *, date_format(convert(SUBSTRING_INDEX(rental_date, ' ', 1), date), '%M %D %Y') as date_is from sakila.rental;
+-- How many rentals were in the last month of activity?;
+
+select count(*) from rental
+where date_format(rental_date, "%Y") = (select date_format(max(rental_date), "%Y") from rental)
+and date_format(rental_date, "%M") = (select date_format(max(rental_date), "%M") from rental);
 
 
-select min(rental_date), max(rental_date), (max(rental_date)-interval 1 month) from sakila.rental;
-
-SELECT * FROM sakila.rental WHERE DATE(return_date) BETWEEN '2006-01-14 15:16:03' and '2006-02-14 15:16:03';
-
-SELECT * FROM sakila.rental WHERE DATE(return_date) BETWEEN max(rental_date) and interval 1 month;
-
-select * from sakila where rental_date > (DATE = '2005-08-02 02:35:22') ;
-
-
+-- NEXT TIME
 
 -- Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week. Check the CASE function.
 dayofweek 1,7 is weekend;
 dayofweek 2-6 is weekday;
 
--- How many rentals were in the last month of activity?;
-SELECT DATA_TYPE from INFORMATION_SCHEMA. COLUMNS where table_schema = 'sakila' and table_name = 'rental';
-SELECT DATA_TYPE from rental COLUMNS where table_schema = 'sakila' and table_name = 'rental_date';
-select datediff(max(rental_date),min(rental_date)) from rental;
-
---------------------
-Try this:
-select count(*) from sakila.rental
-where date_format(rental_date, "%Y") = (select date_format(max(rental_date), "%Y") from sakila.rental)
-and date_format(rental_date, "%M") = (select date_format(max(rental_date), "%M") from sakila.rental);
-This will check the data in the last month of the rentals
-Max(rental_date) is in Feb 2006
-So I am extracting all the results from that month of the yaer
+select *, as time_of_week from rental,
