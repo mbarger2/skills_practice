1da3669049e7616e79386fba45840be33c97baa6
Matthew Barger <matthewbarger2@gmail.com>
2022-09-20 09:10:32 -0400

    more mysql practice

--- sql/Miscellaneous Practice/snippets to study.sql
+++ sql/Miscellaneous Practice/snippets to study.sql
@@ -10,12 +10,52 @@ where k_symbol <> '' and k_symbol <> ' '
 group by type, operation, k_symbol
 having operation <> ''
 order by type, operation, k_symbol;
+-- <> operator is essetially the same as != in that it is looking for all other results than the one given
+show databases;
+use tv_app;
+show tables;
+select title, round(avg(rating),2) from series
+JOIN reviews on reviews.series_id = series.id
+group by title;
+
+select title, rating, count(*) as num_reviews from series
+JOIN reviews on series.id = reviews.series_id
+where rating >= 7 group by title having count(title)>=3;
+
+
 -- window functions
+use bank;
 select loan_id, account_id, amount, payments, duration, amount-payments as "Balance",
 avg(amount-payments) over (partition by duration) as Avg_Balance
 from bank.loan
 where amount > 100000
 order by duration, balance desc;
+
+-- not executable but self-explantory, good structure for partition by
+SELECT
+	year, country, product, profit,
+	SUM(profit) OVER() AS total_profit,
+	SUM(profit) OVER(PARTITION BY country) AS country_profit
+	FROM sales
+	ORDER BY country, year, product, profit;
+
+-- GREAT PARTITION EXAMPLE
+use tv_app;
+Select title, rating, round(avg(rating) over(partition by title),1) as avg_rating from series
+join reviews on series.id = reviews.series_id order by title;
+
+-- rank vs dense_rank
+use tv_app;
+select *, RANK() over(order by rating desc) as tv_ratings from series
+JOIN reviews on series.id = reviews.series_id;
+select *, DENSE_RANK() over(order by rating desc) as tv_ratings from series
+JOIN reviews on series.id = reviews.series_id;
+
+select *, round(avg(rating),2) as avg_rating, RANK() over(order by AVG(rating) desc) as tv_ratings from series
+JOIN reviews on series.id = reviews.series_id GROUP BY title;
+
+
+
 -- You can also have ORDER BY clause followed by partition by as shown below:
 select loan_id, account_id, amount, payments, duration, amount-payments as "Balance",
 avg(amount-payments) over (partition by duration order by duration asc, amounts desc) as Avg_Balance
@@ -30,19 +70,32 @@ set global local_infile = 1;
 load data local infile 'C:/Users/Matthew/DA/Class Notes/Class_14_9-23/district.csv' -- this file is at files_for_lesson_and_activities folder
 into table district_demo
 fields terminated by ',';
-
 select * from district_demo;
 
-
--- FROM 'SQL workbench practice sheet'
+-- failed attempt to upload csv into table due to security restraints
+create database CDs;
+use CDs;
+create table my_cds(
+	Artist varchar(255) DEFAULT 'UNKNOWN',
+    Album  varchar(255)DEFAULT 'NOT FOUND',
+    Year	YEAR default 0000    
+    );
+set global local_infile = 1;
+load data local INFILE 'C:/Users/Owner/Documents/CDscsv.csv' into table my_cds fields terminated by ',' ignore 1 rows;
 
 use bank;
 
 select * from bank.order 
 where k_symbol regexp 'o$';
 
-select * from bank. district 
-where k_sympol regexp '^s';
+
+select * from bank.order 
+where k_symbol regexp 'o+';
+select * from bank.order 
+where k_symbol regexp 'p.' group by k_symbol;
+
+select * from bank.order
+where k_symbol regexp '^s';
 
 select * from bank.district
 where a2 regexp 'cesk[ey]';
@@ -62,7 +115,7 @@ select date_format(convert(date,date), '%Y') from bank.loan;
 
 
 select date_format(convert(SUBSTRING_INDEX(issued, ' ', 1), date), '%M %D, %Y') as year_issued,
-       date_format(convert(SUBSTRING_INDEX(issued, ' ', 1), date), '%d of %M of %Y') as fecha_emision
+       date_format(convert(SUBSTRING_INDEX(issued, ' ', 1), date), '%d of %M of %Y') as fecha_emision;
        
 select isnull('Hello');
 
@@ -79,7 +132,7 @@ where amount is not null;
 select count(isnull(amount)) from bank.trans;
 
 -- FROM 2.08pt1n2 
-
+use bank;
 select *, rank() over (order by A11) as 'rank' from bank.district
 group by A3;
 
